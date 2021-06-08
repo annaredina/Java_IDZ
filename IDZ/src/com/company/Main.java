56bc7e485b61441f5d68e021d1231235794507a4
annaredina <annet.redina@gmail.com>
2021-06-08 16:20:17 +0300

    Fixed

--- IDZ/src/com/company/Main.java
+++ IDZ/src/com/company/Main.java
@@ -3,24 +3,24 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        // Круглое к круглому — всё работает.
-        CofeeJar hole = new CofeeJar(5);
-        Coffee rpeg = new Coffee(5);
-        if (hole.fits(rpeg)) {
+        // Кофе в кофемашину — всё работает.
+        CofeeJar jar = new CofeeJar(5);
+        Coffee cupOfCoffee = new Coffee(5);
+        if (jar.fits(cupOfCoffee)) {
             System.out.println("Coffee with 5% fats fits CoffeeJar");
         }
 
-        Borsh smallSqPeg = new Borsh(2);
-        Borsh largeSqPeg = new Borsh(20);
-        // hole.fits(smallSqPeg); // Не скомпилируется.
+        Borsh smallCup = new Borsh(2);
+        Borsh largeCup = new Borsh(20);
+        // jar.fits(smallCup); // Не скомпилируется.
 
         // Адаптер решит проблему.
-        Adapter smallSqPegAdapter = new Adapter(smallSqPeg);
-        Adapter largeSqPegAdapter = new Adapter(largeSqPeg);
-        if (hole.fits(smallSqPegAdapter)) {
+        Adapter smallAdapter = new Adapter(smallCup);
+        Adapter largeAdapter = new Adapter(largeCup);
+        if (jar.fits(smallAdapter)) {
             System.out.println("Borsh with sourCream 2% fits Coffee 5%");
         }
-        if (!hole.fits(largeSqPegAdapter)) {
+        if (!jar.fits(largeAdapter)) {
             System.out.println("Borsh with sourCream 20% doesnt fit Coffee 5%");
         }
     }
